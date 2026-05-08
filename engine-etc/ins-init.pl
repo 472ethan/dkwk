9de9db525accee6e5043ddc279ea49e69c5692b1
Ethan Meng <uwisc@endfind.me>
2026-05-08 09:21:17 -0500

    add an init installation/refreshment script i like
    just to express my unbridled joy with writing
    perl in UNIX.... :")
    
    (though i ended up just editing with vi on the
    cs400-vm, so whatever)

---
+++ engine-etc/ins-init.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+use Getopt::Long;
+use File::Basename;
+use File::Copy;
+use Fcntl;
+my $usage = "usage: $0 [-O initsys] [-h]";
+my $initsys;
+
+GetOptions(
+	'O=s' => \$initsys,
+	'h' => \my $help)
+	or die $usage;
+
+unless ($initsys) {
+	if ($^O eq 'freebsd') {
+		$initsys = 'freebsd';
+	} elsif ($^O eq 'linux') {
+		my $initlink = -l '/sbin/init' ? basename(readlink('/sbin/init')) : undef;
+		if (!defined $initlink) {
+			die "Your /sbin/init isn't a symlink? How quaint.\n";
+		} elsif ($initlink eq 'openrc-init') {
+			die "I haven't written an openrc script (yet), sorry.\n";
+		} elsif ($initlink eq 'systemd') {
+			die "Just no.\n";
+		} else {
+			die "I am not familiar with your /sbin/init: $initlink\n";
+		}
+	}
+}
+
+if ($initsys eq 'freebsd') {
+	my $bits = 0755;
+	if (my $umask = umask) {
+		$bits &= ~$umask;
+	}
+	my ($source, $target) = qw(dkwk /usr/local/etc/rc.d/dkwk);
+	print STDERR sprintf "+ install %04o %s %s\n",
+		$bits, $source, $target;
+	my $file;
+	sysopen my $file, $target, O_WRONLY|O_CREAT, $bits
+		or die qq[open $target ">": $!\n];
+	binmode $file;
+	copy $source, $file;
+	close $file;
+} else {
+	die "BUG";
+}
